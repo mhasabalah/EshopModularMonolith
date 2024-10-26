61e2bfd9233c570fee376b926af647a781a8cb5e
Mohamed Hasabalah <68168970+mhasabalah@users.noreply.github.com>
2024-10-26 16:39:26 +0300

    Add DDD base classes and interfaces

---
+++ src/Shared/Shared/DDD/IAggregate.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.DDD;
+
+public interface IAggregate<T> : IEntity<T>, IAggregate { }
+
+public interface IAggregate : IEntity
+{
+    IReadOnlyList<IDomainEvent> DomainEvents { get; }
+    IDomainEvent[] ClearDomainEvents();
+}
