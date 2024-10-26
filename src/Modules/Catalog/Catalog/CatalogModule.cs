a24c4cd72176656bc938caca066ab46fbb55f869
Mohamed Hasabalah <68168970+mhasabalah@users.noreply.github.com>
2024-10-26 20:54:57 +0300

    Add Catalog module with EF Core and domain events

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -1,8 +1,4 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace Catalog;
+﻿namespace Catalog;
 
 public static class CatalogModule
 {
--- src/Modules/Catalog/Catalog/GlobalUsing.cs
+++ src/Modules/Catalog/Catalog/GlobalUsing.cs
@@ -1,3 +1,9 @@
-﻿global using Microsoft.AspNetCore.Builder;
+﻿global using Catalog.Products.Events;
+global using Catalog.Products.Models;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
+global using Shared.DDD;
+global using System.Reflection;
